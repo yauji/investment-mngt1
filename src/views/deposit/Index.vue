329730c61d9ca5e98401aa7f0da75211f2f70e97
yoji ozawa <yoji.ozawa@gmail.com>
2021-06-14 23:14:46 +0900

    .

--- src/views/deposit/Index.vue
+++ src/views/deposit/Index.vue
@@ -6,47 +6,73 @@
 
     <table class="table table-striped">
       <thead>
-      <tr>
-        <th>name</th>
-        <th>memo</th>
-        <th>status</th>
-        <th>date</th>
-        <th></th>
-        <th></th>
-        <th></th>
-      </tr>
+        <tr>
+          <th>name</th>
+          <th>memo</th>
+          <th>status</th>
+          <th>date</th>
+
+          <th>principalCurrency</th>
+          <th>principalJPY</th>
+          <th>principalForeign</th>
+          <th>exchangeRate</th>
+          <th>interestRate</th>
+          <th>duration</th>
+
+          <th>endDate</th>
+          <th>valueCurrency</th>
+          <th>valueJPY</th>
+          <th>valueForeign</th>
+          <th></th>
+          <th></th>
+          <th></th>
+        </tr>
       </thead>
       <tbody>
-      <tr v-for="(deposit, index) in deposits" :key="deposit.id">
-        <td>{{ deposit.name }}</td>
-        <td>{{ deposit.memo }}</td>
-        <td>{{ moment(deposit.date) }}</td>
-        <td>{{ deposit.principalCurrency }}</td>
-        <td>{{ deposit.principalForeign }}</td>
-        <td>
-          <router-link
-            custom
-            v-slot="{ navigate }"
-            :to="{ name: 'DepositShow', params: { depositId: deposit.id } }"
-          >
-            <button class="btn btn-primary" @click="navigate">Show</button>
-          </router-link>
-        </td>
-        <td>
-          <router-link
-            custom
-            v-slot="{ navigate }"
-            :to="{ name: 'DepositEdit', params: { depositId: deposit.id } }"
-          >
-            <button class="btn btn-primary" @click="navigate">Edit</button>
-          </router-link>
-        </td>
-        <td>
-          <button class="btn btn-primary" @click="deleteDeposit(index, deposit.id)">
-            Delete 
-          </button>
-        </td>
-      </tr>
+        <tr v-for="(deposit, index) in deposits" :key="deposit.id">
+          <td>{{ deposit.name }}</td>
+          <td>{{ deposit.memo }}</td>
+          <td>{{ deposit.status }}</td>
+          <td>{{ moment(deposit.date) }}</td>
+          <td>{{ deposit.principalCurrency }}</td>
+
+          <td>{{ deposit.principalJPY }}</td>
+          <td>{{ deposit.principalForeign }}</td>
+          <td>{{ deposit.exchangeRate }}</td>
+          <td>{{ deposit.interestRate }}</td>
+          <td>{{ deposit.duration }}</td>
+
+          <td>{{ deposit.endDate }}</td>
+          <td>{{ deposit.valueCurrency }}</td>
+          <td>{{ deposit.valueJPY }}</td>
+          <td>{{ deposit.valueForeign }}</td>
+          <td>
+            <router-link
+              custom
+              v-slot="{ navigate }"
+              :to="{ name: 'DepositShow', params: { depositId: deposit.id } }"
+            >
+              <button class="btn btn-primary" @click="navigate">Show</button>
+            </router-link>
+          </td>
+          <td>
+            <router-link
+              custom
+              v-slot="{ navigate }"
+              :to="{ name: 'DepositEdit', params: { depositId: deposit.id } }"
+            >
+              <button class="btn btn-primary" @click="navigate">Edit</button>
+            </router-link>
+          </td>
+          <td>
+            <button
+              class="btn btn-primary"
+              @click="deleteDeposit(index, deposit.id)"
+            >
+              Delete
+            </button>
+          </td>
+        </tr>
       </tbody>
     </table>
 
@@ -57,7 +83,6 @@
     <router-link custom v-slot="{ navigate }" :to="{ name: 'DepositCreate' }">
       <button class="btn btn-primary" @click="navigate">Add Deposit</button>
     </router-link>
-
   </div>
 </template>
 
@@ -84,10 +109,9 @@ export default {
     };
   },
   methods: {
-    
-   moment: function (date) {
-      return moment(date).format('YYYY/MM/DD')
-//      return moment(date).format('YYYY/MM/DD HH:mm:SS')
+    moment: function (date) {
+      return moment(date).format("YYYY/MM/DD");
+      //      return moment(date).format('YYYY/MM/DD HH:mm:SS')
     },
     async getDeposits() {
       await API.graphql({
