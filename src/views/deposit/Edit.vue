ee1138e98ce6b850b81badac7b5021db7ab4a7b0
yoji ozawa <yoji.ozawa@gmail.com>
2021-06-14 23:55:08 +0900

    .

--- src/views/deposit/Edit.vue
+++ src/views/deposit/Edit.vue
@@ -3,24 +3,139 @@
     <h1>アルバム編集</h1>
     <form @submit.prevent="submitUpdate">
       <label>Name</label><br />
-      <input v-model="form.name" placeholder="Enter album name" /><br />
+      <input v-model="form.name" placeholder="Enter deposit name" /><br />
       <input type="submit" value="Submit" />
     </form>
+
+    <form @submit.prevent="submitUpdate">
+      <input type="submit" value="Submit" />
+
+      <div class="mb-3">
+        <div class="mb-3">
+          <label for="" class="form-label">name *</label>
+          <input
+            type="text"
+            class="form-control"
+            v-model="form.name"
+            required
+          />
+        </div>
+
+        <div class="mb-3">
+          <label for="" class="form-label">memo</label>
+          <textarea class="form-control" v-model="form.memo" />
+        </div>
+
+        <div class="mb-3">
+          <label for="" class="form-label">date</label>
+             <datepicker  v-model="form.date" class="form-control"/>
+          <!--             
+             <datepicker  v-model="picked" />
+          <input type="text" class="form-control" v-model="form.date" />
+          -->
+        </div>
+
+        <div class="mb-3">
+          <label for="" class="form-label">principalCurrency</label>
+          <select
+            class="form-select"
+            aria-label="Default select example"
+            v-model="form.principalCurrency"
+          >
+            <!--
+            <option selected>Open this select menu</option>-->
+            <option value="JPY" selected>JPY</option>
+            <option value="USD">USD</option>
+            <option value="AUD">AUD</option>
+            <option value="EUR">EUR</option>
+            <option value="NZD">NZD</option>
+          </select>
+        </div>
+
+        <div class="mb-3">
+          <label for="" class="form-label">Principal JPY</label>
+          <input type="number" class="form-control" v-model="form.principalJPY" />
+        </div>
+
+        <div class="mb-3">
+          <label for="" class="form-label">Principal Foreign</label>
+          <input type="number" step="0.01" class="form-control" v-model="form.principalForeign" />
+        </div>
+
+        <div class="mb-3">
+          <label for="" class="form-label">exchange rate</label>
+          <input type="number" class="form-control" v-model="form.exchangeRate" />
+        </div>
+
+        <div class="mb-3">
+          <label for="" class="form-label">interest rate</label>
+          <input type="number" class="form-control" v-model="form.interestRate" />
+        </div>
+
+        <div class="mb-3">
+          <label for="" class="form-label">duration</label>
+          <input type="text" class="form-control" v-model="form.duration" />
+        </div>
+
+        <div class="mb-3">
+          <label for="" class="form-label">end date</label>
+                       <datepicker  v-model="form.endDate" class="form-control"/>
+          
+        </div>
+
+        <div class="mb-3">
+          <label for="" class="form-label">valueCurrency</label>
+          <select
+            class="form-select"
+            aria-label="Default select example"
+            v-model="form.valueCurrency"
+          >
+            <option value="JPY" selected>JPY</option>
+            <option value="USD">USD</option>
+            <option value="AUD">AUD</option>
+            <option value="EUR">EUR</option>
+            <option value="NZD">NZD</option>
+          </select>
+        </div>
+
+        <div class="mb-3">
+          <label for="" class="form-label">value JPY</label>
+          <input type="text" class="form-control" v-model="form.valueJPY" />
+        </div>
+
+        <div class="mb-3">
+          <label for="" class="form-label">value foreign</label>
+          <input type="text" class="form-control" v-model="form.valueForeign" />
+        </div>
+
+
+
+      <button type="submit" class="btn btn-primary">Submit</button>
+      </div>
+    </form>    
   </div>
 </template>
 
 <script>
 import { API } from "aws-amplify";
-import { getAlbum } from "../../graphql/queries";
-import { updateAlbum } from "../../graphql/mutations";
+import { getDeposit } from "../../graphql/queries";
+import { updateDeposit } from "../../graphql/mutations";
+
+import Datepicker from "vue3-datepicker";
 
 export default {
-  name: "AlbumEdit",
+  name: "DepositEdit",
+  components: {
+    Datepicker,
+  },  
   props: {
-    albumId: String,
+    depositId: String,
   },
   async created() {
-    this.getAlbum();
+    //console.log(this.props.depositId);
+    //console.log(this.depositId);
+    
+    this.getDeposit();
   },
   data() {
     return {
@@ -31,15 +146,18 @@ export default {
     };
   },
   methods: {
-    async getAlbum() {
+    async getDeposit() {
+      //console.log(this.depositId);
+
       await API.graphql({
-        query: getAlbum,
-        variables: { id: this.albumId },
+        query: getDeposit,
+        variables: { id: this.depositId },
       })
         .then((result) => {
-          console.log(result);
-          this.form.id = result.data.getAlbum.id;
-          this.form.name = result.data.getAlbum.name;
+          //console.log(result);
+          //this.form.id = result.data.getDeposit.id;
+          //this.form.name = result.data.getDeposit.name;
+          this.form = result.data.getDeposit;
         })
         .catch((error) => {
           console.log(error);
@@ -47,12 +165,12 @@ export default {
     },
     async submitUpdate() {
       await API.graphql({
-        query: updateAlbum,
+        query: updateDeposit,
         variables: { input: this.form },
       })
         .then((result) => {
           console.log(result);
-          this.$router.push({ name: "AlbumIndex" });
+          this.$router.push({ name: "DepositIndex" });
         })
         .catch((error) => {
           console.log(error);
