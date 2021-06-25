c59a75fe2241318b4ab190a7013a95a993b17be0
yoji ozawa <yoji.ozawa@gmail.com>
2021-06-25 17:23:12 +0900

    updated exports.

--- src/views/Inputdata.vue
+++ src/views/Inputdata.vue
@@ -2,8 +2,8 @@
   <div>
     <h1>Inputdata</h1>
 
-    deposit finished
-    <br/>
+    <h3>deposit finished</h3>
+    <br />
 
     plus xxx interest rate xxx duration
 
@@ -41,6 +41,31 @@
 
       <!--
 -->
+
+      <input type="submit" value="Submit" />
+    </form>
+
+    <h3>deposit active</h3>
+    <br />
+
+    <form @submit.prevent="submitCreateActive">
+      <div class="mb-3">
+        <label for="" class="form-label">principal account</label>
+        <select
+          class="form-select"
+          aria-label="Default select example"
+          v-model="form.principalAccountId"
+        >
+          <option v-for="n in this.accounts" v-bind:key="n" v-bind:value="n.id">
+            {{ n.currency }} - {{ n.name }}
+          </option>
+        </select>
+      </div>
+
+      <div class="mb-3">
+        <label for="" class="form-label">deposit active</label>
+        <textarea class="form-control" v-model="form.dataDA" />
+      </div>
 
       <input type="submit" value="Submit" />
     </form>
@@ -105,12 +130,12 @@ export default {
         form.date = new Date(arr2[0]);
         form.name = arr2[1];
         form.memo = arr2[7];
-        form.principal = parseFloat(arr2[16].replace(/,/g,""));
+        form.principal = parseFloat(arr2[16].replace(/,/g, ""));
         form.exchangeRate = 0;
-        form.interestRate = parseFloat(arr2[18].replace(/,/g,""));
-        form.duration = parseFloat(arr2[19].replace(/,/g,""));
+        form.interestRate = parseFloat(arr2[18].replace(/,/g, ""));
+        form.duration = parseFloat(arr2[19].replace(/,/g, ""));
         form.endDate = new Date(arr2[12]);
-        form.value = parseFloat(arr2[17].replace(/,/g,""));
+        form.value = parseFloat(arr2[17].replace(/,/g, ""));
 
         form.status = "FINISHED";
 
@@ -118,8 +143,6 @@ export default {
 
         form.principalAccountId = this.form.principalAccountId;
         form.valueAccountId = this.form.valueAccountId;
-
-
 
         //console.log(this.form.date);
         console.log("------", form);
@@ -131,6 +154,44 @@ export default {
           .then((result) => {
             console.log(result);
             //this.$router.push({ name: "DepositIndex" });
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    },
+    async submitCreateActive() {
+      const str = this.form.dataDA;
+
+      var arr = str.split(/\r\n|\n/);
+
+      for (var i = 0; i < arr.length; i++) {
+        var arr2 = arr[i].split(/xxx/);
+        for (const k2 in arr2) {
+          console.log(k2, arr2[k2]);
+        }
+        var form = {};
+
+        form.date = new Date(arr2[0]);
+        form.name = arr2[1];
+        form.memo = arr2[7];
+        form.principal = parseFloat(arr2[16].replace(/,/g, ""));
+        form.exchangeRate = 0;
+        form.interestRate = parseFloat(arr2[18].replace(/,/g, ""));
+        form.duration = parseFloat(arr2[19].replace(/,/g, ""));
+
+        form.status = "ACTIVE";
+
+        form.principalAccountId = this.form.principalAccountId;
+
+        console.log("---active---", form);
+
+        await API.graphql({
+          query: createDeposit,
+          variables: { input: form },
+        })
+          .then((result) => {
+            console.log(result);
           })
           .catch((error) => {
             console.log(error);
